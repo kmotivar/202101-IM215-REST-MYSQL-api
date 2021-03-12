2ad82b804e06b3c236d70fc0cd7bd9aadefcd936
kmotivar <kmotivar@confederationcollege.ca>
2021-03-11 19:02:22 -0500

    Express Hello World Example

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-console.log("Hii")
+// console.log("Hii")
 
-person = 'Keval'
-character = {
-    name: person,
-    age: 23,
-}
+// person = 'Keval'
+// character = {
+//     name: person,
+//     age: 23,
+// }
 
-console.log(person)
-console.log(character)
+// console.log(person)
+// console.log(character)
+
+const express = require('express')
+
+const app = express()
+
+app.get('/', (request, response) => {
+    console.log('Request Received')
+    // response.send('Welcome');
+    response.json({msg: 'Welcome'});
+})
+
+app.listen(3333, () => {
+    console.log("The server is up and listening on port 3333")
+})
