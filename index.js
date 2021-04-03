895c0d0f5d20172c0d4e0c19b6fefb09a78827b2
kmotivar <kmotivar@confederationcollege.ca>
2021-04-03 18:08:00 -0400

    latest nodemon nd morgan

--- index.js
+++ index.js
@@ -9,15 +9,52 @@
 // console.log(person)
 // console.log(character)
 
+const { response } = require('express')
 const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+
 
 const app = express()
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.get('/', (request, response) => {
-    console.log('Request Received')
+    //console.log('Request Received')
     // response.send('Welcome');
-    response.json({msg: 'Welcome'});
+    // response.json({msg: 'Welcome'});
+
+    /* 
+        Types of Parameters:
+        * Query param1=val1&param2=val2
+        * Route localhost:3333/name/val1/age/val2/birthday/val3...
+        * Body param1=val1&param2=val2 (if using form)
+    */
+
+    // Query
+    // console.log("Query parameters")
+    // console.log(request.query)
+
+    // Route
+    // this is different...use
+    // app.get('/:first_name/:last_name/:age', (request, response) => {
+    //         console.log(request.params)
+    //          response.end()
+    //     })
+
+    // Body 
+    // console.log('Body parameters')
+    // console.log(request.body)
+
+    response.end()
+
 })
+
+// for route parameter
+// app.get('/:first_name/:last_name/:age', (request, response) => {
+//             console.log(request.params)
+//             response.end()
+//         })
 
 app.listen(3333, () => {
     console.log("The server is up and listening on port 3333")
